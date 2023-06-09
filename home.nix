cfc3830a466131ed509a779f9086f13062978709
Jesse Hallett <jesse@sitr.us>
2023-06-09 15:36:26 -0400

    configure firefox with my full-screen-to-hide-chrome tweak

--- home.nix
+++ home.nix
@@ -38,6 +38,15 @@
     neovim
   ];
 
+  programs.firefox = {
+    enable = true;
+    profiles.default.settings = {
+      # Full-screen does not actually make the window full screen. I use this
+      # setting to abuse full-screen mode to hide the navigation and tab bars.
+      "full-screen-api.ignore-widgets" = true;
+    };
+  };
+
   programs.git = {
     enable = true;
     userName = "Jesse Hallett";
