742a7f437952c7fcdf908eb235b97600567b8249
Jesse Hallett <jesse@sitr.us>
2023-06-11 22:22:59 -0400

    find Gnome extension packages automatically

--- gnome/default.nix
+++ gnome/default.nix
@@ -1,10 +1,48 @@
 { config, pkgs, ... }:
 
+let
+  # Each Gnome extension has an email-like uuid. Gnome uses these uuids to track
+  # which of the installed extensions are enabled.
+  #
+  # There are a couple of ways to find the uuid. If you look at the source for
+  # an extension it should have a `metadata.json` with a `uuid` field. Or on
+  # a gnome.extensions.org page you can look at the HTML source and find a div
+  # with an attribute like, `data-uuid="..."`.
+  enabledExtensions = [
+    "advanced-alt-tab@G-dH.github.com"
+    "appindicatorsupport@rgcjonas.gmail.com"
+    "gtktitlebar@velitasali.github.io"
+    "horizontal-workspace-indicator@tty2.io"
+    "paperwm@hedning:matrix.org"
+    "runcat@kolesnikov.se"
+  ];
+
+  # Search through `pkgs.gnomeExtensions` for packages for each extension that
+  # we want. Packages are found by checking the package's `extensionUuid`
+  # attribute against the list of enabled extension uuids.
+  enabledExtensionPackages =
+    let
+      allExtensionPackages = builtins.attrValues pkgs.gnomeExtensions;
+      isEnabled = extensionPackage:
+        let p = builtins.tryEval extensionPackage; # tryEval because some packages evaluate to deprecation errors
+        in
+        p.success && builtins.isAttrs p.value && builtins.hasAttr "extensionUuid" p.value &&
+        builtins.any (e: p.value.extensionUuid == e) enabledExtensions;
+    in
+    checkForMissing (builtins.filter isEnabled allExtensionPackages);
+
+  checkForMissing = extensionPackages:
+    let
+      noPackagesMatchUuid = uuid: !(builtins.any (p: p.extensionUuid == uuid) extensionPackages);
+      missingUuids = builtins.filter noPackagesMatchUuid enabledExtensions;
+    in
+    if missingUuids != [ ] then abort "Could not find packages for some Gnome extensions: ${builtins.toString missingUuids}"
+    else extensionPackages;
+in
 {
   dconf.settings = {
     "org/gnome/desktop/interface" = {
       color-scheme = "prefer-dark";
-      #enable-hot-corners = false;
     };
 
     # Settings that PaperWM prefers
@@ -16,21 +54,18 @@
 
     "org/gnome/shell" = {
       disable-user-extensions = false;
-      enabled-extensions = [
-        "advanced-alt-tab@G-dH.github.com"
-        "appindicatorsupport@rgcjonas.gmail.com"
-        "gtktitlebar@velitasali.github.io"
-        "paperwm@hedning:matrix.org"
-        "runcat@kolesnikov.se"
-      ];
+      enabled-extensions = enabledExtensions;
+    };
+
+    "org/gnome/shell/extensions/paperwm/keybindings" = {
+      "switch-left" = [ "<Super>Left" "<Super>h" ];
+      "switch-right" = [ "<Super>Right" "<Super>n" ];
+      "switch-up" = [ "<Super>Up" "<Super>c" ];
+      "switch-down" = [ "<Super>Down" "<Super>t" ];
+      "toggle-scratch" = [ "<Shift><Super>quotedbl" ]; # move window into or out of scratch layer
+      "toggle-scratch-layer" = [ "<Super>apostrophe" ];
     };
   };
 
-  home.packages = with pkgs.gnomeExtensions; [
-    advanced-alttab-window-switcher
-    appindicator
-    paperwm
-    gtk-title-bar
-    runcat
-  ];
+  home.packages = enabledExtensionPackages;
 }
