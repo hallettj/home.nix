0e80ed6acae7f5feafa0969d7096a7d67811235d
Jesse Hallett <jesse@sitr.us>
2023-06-17 14:02:25 -0400

    reorganize neovim configuration a little just because

--- neovim/default.nix
+++ neovim/default.nix
@@ -1,23 +1,33 @@
 { config, pkgs, lib, ... }:
 
 let
+  # Out-of-store symlinks require absolute paths when using a flake config. This
+  # is because relative paths are expanded after the flake source is copied to
+  # a store path which would get us read-only store paths.
   dir = "${config.xdg.configHome}/home-manager/neovim";
 in
 {
+  xdg.configFile.nvim = {
+    source = config.lib.file.mkOutOfStoreSymlink "${dir}/nvim-config";
+  };
+
   programs.neovim = {
     enable = true;
     defaultEditor = true;
     withPython3 = true;
-  };
+    extraPackages = with pkgs; [
+      fd
+      gh # for github integration
+      ripgrep
 
-  home.file = {
-    # Symlink config files in this directory. An absolute path is required when
-    # using a flake config from home-manager because relative paths are
-    # expanded after the flake source is copied to a store path.
-    nvim-config = {
-      source = config.lib.file.mkOutOfStoreSymlink "${dir}/nvim-config";
-      target = ".config/nvim";
-    };
+      # needed to compile fzf-native for telescope-fzf-native.nvim
+      gcc
+      gnumake
+
+      # language servers
+      rnix-lsp
+      lua-language-server
+    ];
   };
 
   home.sessionVariables = {
@@ -32,18 +42,4 @@ in
     # I just don't want extra UI anywhere
     NVIM_GTK_NO_HEADERBAR = "1";
   };
-
-  home.packages = with pkgs; [
-    fd
-    gh # for github integration
-    ripgrep
-
-    # needed to compile fzf-native for telescope-fzf-native.nvim
-    gcc
-    gnumake
-
-    # language servers
-    rnix-lsp
-    lua-language-server
-  ];
 }
