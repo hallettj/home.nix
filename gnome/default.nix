c1b18a928e38288011c6cdfc5951e696e3550bee
Jesse Hallett <jesse@sitr.us>
2023-06-17 17:31:12 -0400

    move gnome extensions setup into a reusable module

--- gnome/default.nix
+++ gnome/default.nix
@@ -1,44 +1,6 @@
 { config, pkgs, ... }:
 
 let
-  # Each Gnome extension has an email-like uuid. Gnome uses these uuids to track
-  # which of the installed extensions are enabled.
-  #
-  # There are a couple of ways to find the uuid. If you look at the source for
-  # an extension it should have a `metadata.json` with a `uuid` field. Or on
-  # a gnome.extensions.org page you can look at the HTML source and find a div
-  # with an attribute like, `data-uuid="..."`.
-  enabledExtensions = [
-    "advanced-alt-tab@G-dH.github.com"
-    "appindicatorsupport@rgcjonas.gmail.com"
-    "gtktitlebar@velitasali.github.io"
-    "horizontal-workspace-indicator@tty2.io"
-    "paperwm@hedning:matrix.org"
-    "runcat@kolesnikov.se"
-  ];
-
-  # Search through `pkgs.gnomeExtensions` for packages for each extension that
-  # we want. Packages are found by checking the package's `extensionUuid`
-  # attribute against the list of enabled extension uuids.
-  enabledExtensionPackages =
-    let
-      allExtensionPackages = builtins.attrValues pkgs.gnomeExtensions;
-      isEnabled = extensionPackage:
-        let p = builtins.tryEval extensionPackage; # tryEval because some packages evaluate to deprecation errors
-        in
-        p.success && builtins.isAttrs p.value && builtins.hasAttr "extensionUuid" p.value &&
-        builtins.any (e: p.value.extensionUuid == e) enabledExtensions;
-    in
-    checkForMissing (builtins.filter isEnabled allExtensionPackages);
-
-  checkForMissing = extensionPackages:
-    let
-      noPackagesMatchUuid = uuid: !(builtins.any (p: p.extensionUuid == uuid) extensionPackages);
-      missingUuids = builtins.filter noPackagesMatchUuid enabledExtensions;
-    in
-    if missingUuids != [ ] then abort "Could not find packages for some Gnome extensions: ${builtins.toString missingUuids}"
-    else extensionPackages;
-
   paperwmWorkspaces =
     [
       { name = "Home"; id = "f7bf4f04-df01-49d4-90d9-ac80b7e9d3b2"; }
@@ -48,7 +10,21 @@ let
     ];
 in
 {
-  dconf.settings = {
+  imports = [ ./gnome-extensions.nix ];
+
+  config.gnomeExtensions = {
+    enable = true;
+    enabledExtensionUuids = [
+      "advanced-alt-tab@G-dH.github.com"
+      "appindicatorsupport@rgcjonas.gmail.com"
+      "gtktitlebar@velitasali.github.io"
+      "horizontal-workspace-indicator@tty2.io"
+      "paperwm@hedning:matrix.org"
+      "runcat@kolesnikov.se"
+    ];
+  };
+
+  config.dconf.settings = {
     "org/gnome/desktop/calendar".show-weekdate = true;
     "org/gnome/desktop/interface" = {
       color-scheme = "prefer-dark";
@@ -69,8 +45,6 @@ in
     };
 
     "org/gnome/shell" = {
-      disable-user-extensions = false;
-      enabled-extensions = enabledExtensions;
       favorite-apps = [
         "org.gnome.Nautilus.desktop"
         "org.gnome.Software.desktop"
@@ -204,6 +178,4 @@ in
         })
         paperwmWorkspaces)
   );
-
-  home.packages = enabledExtensionPackages;
 }
---
+++ gnome/gnome-extensions.nix
@@ -0,0 +1,97 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+let
+  cfg = config.gnomeExtensions;
+
+  # Search through `pkgs.gnomeExtensions` for packages for each extension that
+  # we want. Packages are found by checking the package's `extensionUuid`
+  # attribute against the list of enabled extension uuids.
+  enabledExtensionPackages =
+    let
+      allExtensionPackages = builtins.attrValues pkgs.gnomeExtensions;
+
+      uuidMatches = extensionPackage:
+        let packageUuid = extensionUuid extensionPackage;
+        in
+        uuid: packageUuid == uuid;
+
+      hasPackageOverride = uuid: builtins.any (package: uuidMatches package uuid) cfg.packages;
+
+      uuidsWithoutOverride = builtins.filter (uuid: !(hasPackageOverride uuid)) cfg.enabledExtensionUuids;
+
+      isEnabled = extensionPackage:
+        builtins.any (uuidMatches extensionPackage) uuidsWithoutOverride;
+    in
+    builtins.filter isEnabled allExtensionPackages;
+
+  extensionUuid = extensionPackage:
+    let p = builtins.tryEval extensionPackage; # tryEval because some packages evaluate to deprecation errors
+    in
+    if p.success && builtins.isAttrs p.value && builtins.hasAttr "extensionUuid" p.value
+    then p.value.extensionUuid
+    else null;
+
+  checkForMissing = extensionPackages:
+    let
+      noPackagesMatchUuid = uuid: !(builtins.any (p: extensionUuid p == uuid) extensionPackages);
+      missingUuids = builtins.filter noPackagesMatchUuid cfg.enabledExtensionUuids;
+    in
+    if missingUuids != [ ] then abort "Could not find packages for some Gnome extensions: ${builtins.toString missingUuids}"
+    else extensionPackages;
+
+  checkForExtensionUuids = extensionPackages:
+    let
+      isAttributeMissing = extensionPackage: builtins.isNull (extensionUuid extensionPackage);
+      packagesMissingAttribute = builtins.filter isAttributeMissing extensionPackages;
+    in
+    if packagesMissingAttribute != [ ] then
+      builtins.throw "Some of the given extension packages do not have an `extensionUuid` attribute: ${buitins.toString packagesMissingAttribute}"
+    else extensionPackages;
+in
+{
+  options.gnomeExtensions = {
+    enable = mkEnableOption "gnome-extensions";
+
+    enabledExtensionUuids = mkOption {
+      type = types.listOf types.str;
+      description = ''
+        Provide a list of Gnome extension UUID strings. These will be set as the
+        list of enabled extensions. The corresponding packages will
+        automatically be installed from pkgs.gnomeExtensions.
+
+        Each Gnome extension has an email-like UUID. Gnome uses these UUIDs to
+        track which of the installed extensions are enabled. There are a couple of
+        ways to find the UUID. If you look at the source for an extension it
+        should have a `metadata.json` with a `uuid` field. Or on
+        a gnome.extensions.org page you can look at the HTML source and find a div
+        with an attribute like, `data-uuid="..."`.
+      '';
+    };
+
+    packages = mkOption {
+      type = types.listOf types.package;
+      default = [ ];
+      description = ''
+        Any extension packages listed here will be used instead of packages from
+        pkgs.gnomeExtensions.
+
+        Each package must have an `extensionUuid` attribute that matches one of
+        the UUIDs listed in the `enabledExtensionUuids` option. If you build
+        a package using `stdenv.mkDerivation` then you can do this by passing an
+        attribute called `passthru.extensionUuid` to `mkDerivation`.
+      '';
+    };
+  };
+
+  config = mkIf cfg.enable {
+    dconf.settings = {
+      "org/gnome/shell" = {
+        disable-user-extensions = false;
+        enabled-extensions = cfg.enabledExtensionUuids;
+      };
+    };
+    home.packages = checkForMissing (enabledExtensionPackages ++ checkForExtensionUuids cfg.packages);
+  };
+}
