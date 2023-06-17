05c15bdb8c05a7b5fdacbbb03f90141b309c3365
Jesse Hallett <jesse@sitr.us>
2023-06-17 14:02:06 -0400

    configure paperwm, and some other gnome-related settings

--- gnome/default.nix
+++ gnome/default.nix
@@ -38,12 +38,19 @@ let
     in
     if missingUuids != [ ] then abort "Could not find packages for some Gnome extensions: ${builtins.toString missingUuids}"
     else extensionPackages;
+
+  paperwmWorkspaces =
+    [
+      { name = "Home"; id = "f7bf4f04-df01-49d4-90d9-ac80b7e9d3b2"; }
+      { name = "Work"; id = "d13c7071-e870-4014-92a4-709476e62916"; color = "rgb(73,64,102)"; }
+      { name = "IBNU"; id = "7c060a9f-4e62-40c6-9d78-278d8d959e2c"; }
+      { name = "Games"; id = "e47bab66-3212-415e-bc03-cd4cd6cef712"; }
+    ];
 in
 {
   dconf.settings = {
-    "org/gnome/desktop/interface" = {
-      color-scheme = "prefer-dark";
-    };
+    "org/gnome/desktop/calendar".show-weekdate = true;
+    "org/gnome/desktop/interface".color-scheme = "prefer-dark";
 
     # Settings that PaperWM prefers
     "org/gnome/mutter" = {
@@ -57,15 +64,85 @@ in
       enabled-extensions = enabledExtensions;
     };
 
-    "org/gnome/shell/extensions/paperwm/keybindings" = {
-      "switch-left" = [ "<Super>Left" "<Super>h" ];
-      "switch-right" = [ "<Super>Right" "<Super>n" ];
-      "switch-up" = [ "<Super>Up" "<Super>c" ];
-      "switch-down" = [ "<Super>Down" "<Super>t" ];
-      "toggle-scratch" = [ "<Shift><Super>quotedbl" ]; # move window into or out of scratch layer
-      "toggle-scratch-layer" = [ "<Super>apostrophe" ];
+    "org/gnome/shell/extensions/paperwm" = {
+      cycle-width-steps = [ 0.25 0.33329999999999999 0.5 0.66659999999999997 0.75 ];
+      horizontal-margin = 8;
+      show-window-position-bar = true;
+      topbar-follow-focus = true;
+      vertical-margin-bottom = 2;
+      window-gap = 8;
+      winprops = [
+        ''{"wm_class":"kitty","preferredWidth":"25%"}''
+        ''{"wm_class":"neovide","preferredWidth":"75%"}''
+        ''{"wm_class":"Slack","scratch_layer":true}''
+        ''{"wm_class":"","title":"YouTube Music","scratch_layer":true}''
+      ];
     };
-  };
+
+    "org/gnome/shell/extensions/paperwm/keybindings" = {
+      center-horizontally = [ "<Super>m" ];
+      live-alt-tab = [ "" ];
+      move-down = [ "<Shift><Super>Down" "<Shift><Super>t" ];
+      move-down-workspace = [ "<Shift><Super>Page_Down" ];
+      move-left = [ "<Shift><Super>Left" "<Shift><Super>h" ];
+      move-monitor-above = [ "" ];
+      move-monitor-below = [ "" ];
+      move-monitor-left = [ "" ];
+      move-monitor-right = [ "" ];
+      move-previous-workspace = [ "" ];
+      move-previous-workspace-backward = [ "" ];
+      move-right = [ "<Shift><Super>Right" "<Shift><Super>n" ];
+      move-up = [ "<Shift><Super>Up" "<Shift><Super>c" ];
+      move-up-workspace = [ "<Shift><Super>Page_Up" ];
+      new-window = [ "<Super>Return" ];
+      previous-workspace = [ "" ];
+      previous-workspace-backward = [ "" ];
+      switch-down = [ "<Super>Down" "<Super>t" ];
+      switch-focus-mode = [ "<Shift><Super>m" ];
+      switch-left = [ "<Super>Left" "<Super>h" ];
+      switch-monitor-above = [ "" ];
+      switch-monitor-below = [ "" ];
+      switch-monitor-left = [ "" ];
+      switch-monitor-right = [ "" ];
+      switch-right = [ "<Super>Right" "<Super>n" ];
+      switch-up = [ "<Super>Up" "<Super>c" ];
+      take-window = [ "<Super>b" ];
+      toggle-scratch = [ "<Shift><Super>quotedbl" ]; # move window into or out of scratch layer
+      toggle-scratch-layer = [ "<Control><Super>apostrophe" ];
+      toggle-scratch-window = [ "<Super>apostrophe" ];
+    };
+
+    "org/gnome/shell/extensions/runcat" = {
+      displaying-items = "character-only";
+    };
+
+    "org/gnome/GWeather" = {
+      temperature-unit = "centigrade";
+    };
+
+    "org/gnome/shell/extensions/paperwm/workspaces" = {
+      list = builtins.map (w: w.id) paperwmWorkspaces;
+    };
+  } // (
+    # Transform the `paperwmWorkspaces` list into a set of dconf values of the
+    # form,
+    #
+    #     "d13c7071-e870-4014-92a4-709476e62916" = {
+    #       name = "Work";
+    #       index = 1;
+    #       color = "rgb(73,64,102)";
+    #     };
+    #
+    builtins.listToAttrs
+      (pkgs.lib.lists.imap0
+        (index: workspace: {
+          name = "org/gnome/shell/extensions/paperwm/workspaces/${workspace.id}";
+          value = builtins.removeAttrs workspace [ "id" ] // { index = index; };
+        })
+        paperwmWorkspaces)
+  );
+
+  # TODO: gnome key bindings
 
   home.packages = enabledExtensionPackages;
 }
