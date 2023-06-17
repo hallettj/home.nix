af84909ff20c1d1553d92158a389066846961f03
Jesse Hallett <jesse@sitr.us>
2023-06-17 14:58:35 -0400

    install and configure kitty

--- flake.nix
+++ flake.nix
@@ -24,6 +24,7 @@
         modules = [
           ./home.nix
           ./gnome
+          ./kitty
           ./neovim
           ./zsh
         ];
---
+++ kitty/default.nix
@@ -0,0 +1,22 @@
+{ config, pkgs, ... }:
+
+let
+  # Out-of-store symlinks require absolute paths when using a flake config. This
+  # is because relative paths are expanded after the flake source is copied to
+  # a store path which would get us read-only store paths.
+  dir = "${config.xdg.configHome}/home-manager/kitty";
+in
+{
+  home.file.kitty-config = {
+    source = config.lib.file.mkOutOfStoreSymlink "${dir}/kitty-config";
+    target = ".config/kitty";
+  };
+
+  home.packages = with pkgs; [
+    kitty
+    kitty-themes
+
+    # Need a nerdfont to get icons. Another option could be "NerdFontsSymbolsOnly".
+    (nerdfonts.override { fonts = [ "FiraCode" ]; })
+  ];
+}
