6f720c28c0c2adc1b95a1f4697675d2ceea7298f
Jesse Hallett <jesse@sitr.us>
2023-07-10 11:12:47 -0700

    install docker; I also enabled the daemon in my nixos config

--- home.nix
+++ home.nix
@@ -22,23 +22,6 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
-
     # GUI apps
     authy
     discord
@@ -50,6 +33,7 @@
 
     # Programming
     cargo
+    docker
     rustc
     rust-analyzer
     clang
