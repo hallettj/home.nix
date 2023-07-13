ba001c810bd5357c2386baed407af0bbf371e897
Jesse Hallett <jesse@sitr.us>
2023-07-13 12:43:06 -0700

    add my XCompose file

--- home.nix
+++ home.nix
@@ -99,19 +99,8 @@
     ignores = [ "*.swo" "*.swp" ];
   };
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
+    ".XCompose".source = dotfiles/XCompose;
   };
 
   # You can also manage environment variables but you will have to manually
