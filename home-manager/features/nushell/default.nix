a26c151d5e9e81f3f648db4e0a322a09b8e750a1
Jesse Hallett <jesse@sitr.us>
2023-09-25 11:57:19 -0700

    get some completions and custom commands from nu_scripts

--- home-manager/features/nushell/default.nix
+++ home-manager/features/nushell/default.nix
@@ -2,12 +2,35 @@
 
 let
   dir = "${flakePath config}/home-manager/features/nushell";
+
+  nu_scripts = pkgs.unstable.nu_scripts;
+  use_completions = cmds:
+    let
+      imports =
+        builtins.map (cmd: "use ${nu_scripts}/share/nu_scripts/custom-completions/${cmd}/${cmd}-completions.nu *") cmds;
+    in
+    builtins.concatStringsSep "\n" imports;
 in
 {
   programs.nushell = {
     enable = true;
     envFile.text = "source ${dir}/env.nu";
-    configFile.text = "source ${dir}/config.nu";
+    configFile.text = let nu_scripts = pkgs.unstable.nu_scripts; in "
+      ${use_completions [
+        "cargo"
+        "git"
+        "make"
+        "man" 
+        "nix"
+        "npm"
+      ]}
+      use ${nu_scripts}/share/nu_scripts/custom-completions/yarn/yarn-completion.nu *
+
+      use ${nu_scripts}/share/nu_scripts/modules/filesystem/expand.nu
+      use ${nu_scripts}/share/nu_scripts/modules/nix/nix.nu *
+
+      source ${dir}/config.nu
+    ";
   };
 
   # I'm not setting nushell as my login shell because it is not Posix, and it is
@@ -20,9 +43,11 @@ in
   # Manages and synchronizes shell history
   programs.atuin = {
     enable = true;
+    flags = [ "--disable-up-arrow" ];
     package = pkgs.unstable.atuin;
     settings = {
       inline_height = 16;
+      search_mode = "skim";
       show_help = false;
       update_check = false;
       workspaces = true;
