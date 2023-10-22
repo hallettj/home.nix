b46365727c833dacca73aba0cfbdddf7287476df
Jesse Hallett <jesse@sitr.us>
2023-10-22 12:59:03 -0700

    the garnix binary cache sets its own priority

--- nixos/features/garnix/default.nix
+++ nixos/features/garnix/default.nix
@@ -2,7 +2,7 @@
 
 {
   nix.settings = {
-    substituters = [ "https://cache.garnix.io?priority=50" ]; # cache.nixos.org priority is 40 so it will be hit first
+    substituters = [ "https://cache.garnix.io" ];
     trusted-public-keys = [ "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g=" ];
   };
 }
