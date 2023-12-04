3a95529ce91b1960af683f988de669f6bf787d77
Jesse Hallett <jesse@sitr.us>
2023-12-04 13:57:00 -0800

    fix path to ssh config

--- home-manager/features/ssh/default.nix
+++ home-manager/features/ssh/default.nix
@@ -4,7 +4,7 @@ let
   # Out-of-store symlinks require absolute paths when using a flake config. This
   # is because relative paths are expanded after the flake source is copied to
   # a store path which would get us read-only store paths.
-  dir = "${flakePath config}/home-manager/common/ssh";
+  dir = "${flakePath config}/home-manager/features/ssh";
   symlink = path: config.lib.file.mkOutOfStoreSymlink "${dir}/${path}";
 in
 {
