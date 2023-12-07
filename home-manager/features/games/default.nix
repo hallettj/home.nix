d4848f7aee32ea67bb4c9579f68dfb61ede89789
Jesse Hallett <jesse@sitr.us>
2023-12-06 16:33:45 -0800

    add note on upgrade steps for minecraft

--- home-manager/features/games/default.nix
+++ home-manager/features/games/default.nix
@@ -11,10 +11,13 @@
     # Alternative Minecraft launcher. This is installed with a version of
     # openjdk that is compatible with Minecraft, and the launcher is run with
     # a flag that adds the openjdk store path to the launcher's java search
-    # path. I think the way to go to make this work well is to not mess with the
-    # java installation path. But on upgrades when the previous openjdk store
-    # path is gc'ed it might be necessary to update the java path setting.
-    unstable.prismlauncher
+    # path. On updates that change the bundled openjdk store path:
+    #
+    # - run Prism
+    # - open Settings > Java
+    # - click Auto-detect...
+    # - select the 17.xx option
+    prismlauncher
 
     # steam is installed by a nixos module
     vulkan-tools # just to check that vulkan is set up
