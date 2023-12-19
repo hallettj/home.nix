b577b9f69b532a8065fd696812ec27f9e6bab72d
Jesse Hallett <jesse@sitr.us>
2023-12-19 11:41:20 -0800

    install cmdline completion source so completions actually work

--- home-manager/features/neovim/nvim-config/lua/plugins/cmp.lua
+++ home-manager/features/neovim/nvim-config/lua/plugins/cmp.lua
@@ -3,6 +3,7 @@ return {
   'hrsh7th/nvim-cmp',
   dependencies = {
     'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-cmdline',
     'hrsh7th/cmp-path',
     'hrsh7th/cmp-nvim-lsp',
     'hrsh7th/cmp-nvim-lua',
