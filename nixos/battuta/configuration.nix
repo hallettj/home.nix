5b751177c0f40c0a8dcec8978b90bb4272828a57
Jesse Hallett <jesse@sitr.us>
2023-12-27 18:46:28 -0800

    on suspend enter S3 deep sleep state instead of shallow s2idle state

--- nixos/battuta/configuration.nix
+++ nixos/battuta/configuration.nix
@@ -16,6 +16,9 @@
   boot.initrd.luks.devices."luks-4c48c282-3cfc-4a31-a9bd-ae36ddb8fe1b".device = "/dev/disk/by-uuid/4c48c282-3cfc-4a31-a9bd-ae36ddb8fe1b";
   boot.initrd.luks.devices."luks-4c48c282-3cfc-4a31-a9bd-ae36ddb8fe1b".keyFile = "/crypto_keyfile.bin";
 
+  # On suspend enter S3 deep sleep state instead of shallow s2idle state
+  boot.kernelParams = ["mem_sleep_default=deep"];
+
   networking.hostName = "battuta";
 
   virtualisation.docker.enable = true;
