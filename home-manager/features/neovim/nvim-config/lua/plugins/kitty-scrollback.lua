7d786620a0fc463c30f27b93d842132bd5eb54b1
Jesse Hallett <jesse@sitr.us>
2024-01-16 11:27:58 -0800

    install kitty-scrollback.nvim

---
+++ home-manager/features/neovim/nvim-config/lua/plugins/kitty-scrollback.lua
@@ -0,0 +1,6 @@
+return {
+  'mikesmithgh/kitty-scrollback.nvim',
+  cmd = { 'KittyScrollbackGenerateKittens', 'KittyScrollbackCheckHealth' },
+  event = { 'User KittyScrollbackLaunch' },
+  opts = {},
+}
