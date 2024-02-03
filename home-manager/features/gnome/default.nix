5c4be0457584ae93d9cac6dd57f7ac006f62f901
Jesse Hallett <jesse@sitr.us>
2024-02-03 13:59:59 -0800

    change

--- home-manager/features/gnome/default.nix
+++ home-manager/features/gnome/default.nix
@@ -53,6 +53,7 @@ in
     };
 
     "org/gnome/settings-daemon/plugins/media-keys" = {
+      "screensaver" = [ "<Control><Super>l" ];
       # Makes media keys change volume by a smaller increment
       "volume-step" = 2;
     };
