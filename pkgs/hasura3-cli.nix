a1b187f41d4dd30541f22d8f1922b9937bb3f269
Jesse Hallett <jesse@sitr.us>
2024-02-15 11:00:01 -0800

    upgrade hasura3 cli to 2024.01.24

--- pkgs/hasura3-cli.nix
+++ pkgs/hasura3-cli.nix
@@ -9,22 +9,22 @@ let
   #     $ nix store prefetch-file <url>
   #
   # Make sure to preserve the "sha256-" prefix when pasting here.
-  version = "2024.01.15";
+  version = "2024.01.24";
   src =
     if hostPlatform.system == "x86_64-linux" then
       {
         url = "https://graphql-engine-cdn.hasura.io/ddn/cli/${version}/cli-hasura3-linux-amd64";
-        hash = "sha256-Y0ZeixmQ+MYm7GYZPLKp4gpNonyQ0xFbjAg23zdTm6I=";
+        hash = "sha256-4cvJT2KplGc6qpfVQJUsdlpKXK1017qymVZU649bQQc=";
       }
     else if hostPlatform.system == "x86_64-darwin" then
       {
         url = "https://graphql-engine-cdn.hasura.io/ddn/cli/${version}/cli-hasura3-darwin-amd64";
-        hash = "sha256-tYRfTp+xLWbb8bDY+e79grhneV0bB2S76nb0kSJIzLc=";
+        hash = "sha256-qtC92XkysxpgXLbiZAAvmXFXSZ/qivKiv3F/jvgYPj4=";
       }
     else if hostPlatform.system == "aarch64-darwin" then
       {
         url = "https://graphql-engine-cdn.hasura.io/ddn/cli/${version}/cli-hasura3-darwin-arm64";
-        hash = "sha256-AuERNYOUSSklkbYRNXTgrL6EXz/DGQStgNUpWJCPaH0=";
+        hash = "sha256-N64nY3eVOGcu26H0vrNtXYtGq4MF7RUIoz6A5TsPjwk=";
       }
     else
       builtins.throw "no hasuar3 cli builds available for system, ${hostPlatform.system}"
