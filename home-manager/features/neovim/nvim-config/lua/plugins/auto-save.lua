4ea9d84a3d57a31f4ea360bcc9e0ea43ed9cc3b9
Jesse Hallett <jesse@sitr.us>
2024-02-18 16:59:43 -0800

    consolidate auto-save configuration

--- home-manager/features/neovim/nvim-config/lua/plugins/auto-save.lua
+++ home-manager/features/neovim/nvim-config/lua/plugins/auto-save.lua
@@ -1,13 +1,60 @@
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+
 return {
   '907th/vim-auto-save',
   init = function()
     vim.g.auto_save = 1
     vim.g.auto_save_silent = 1
 
+    -- Automatically save when the window loses focus or when a buffer is
+    -- hidden.
+    vim.o.autoread = true
+    vim.o.hidden = true
+
+    -- Don't keep swap files.
+    vim.o.swapfile = false
+    vim.o.backup = false
+    vim.o.writebackup = true
+
+    -- Keep backups for writebackup in a central location
+    vim.o.backupdir = vim.fn.stdpath('data') .. '/backup//'
+
+    -- This isn't used since noswapfile is set, but it's here for symmetry
+    vim.o.directory = vim.fn.stdpath('data') .. '/swap//'
+
+    -- Reduce time to, e.g., CursorHold event
+    vim.o.updatetime = 300
+
+    local group = augroup('my_autosave', { clear = true })
+
+    -- Trigger `autoread` when files changes on disk
+    -- https://unix.stackexchange.com/questions/149209/refresh-changed-content-of-file-opened-in-vim/383044#383044
+    -- https://vi.stackexchange.com/questions/13692/prevent-focusgained-autocmd-running-in-command-line-editing-mode
+    autocmd({ 'FocusGained', 'BufEnter' }, {
+      group = group,
+      pattern = '*',
+      callback = function()
+        if (vim.api.nvim_get_mode().mode ~= 'c') then
+          vim.cmd.checktime()
+        end
+      end
+    })
+
+    -- Notification after file change
+    -- https://vi.stackexchange.com/questions/13091/autocmd-event-for-autoread
+    autocmd('FileChangedShellPost', {
+      group = group,
+      pattern = '*',
+      callback = function()
+        vim.api.nvim_echo({ { 'File changed on disk. Buffer reloaded.', 'WarningMsg' } }, false, {})
+      end
+    })
+
     -- Disable autosave in Cargo.toml - constant workspace reloading is too
     -- disruptive.
-    vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
-      group = vim.api.nvim_create_augroup('vim-autosave-hooks', { clear = true }),
+    autocmd({ 'BufNewFile', 'BufRead' }, {
+      group = group,
       pattern = 'Cargo.toml',
       callback = function()
         vim.b.auto_save = 0
