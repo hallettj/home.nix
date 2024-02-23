f24d08e1d760fb80e94339f13abca8c5383bdabf
Jesse Hallett <jesse@sitr.us>
2024-02-23 14:53:30 -0800

    upgrade warp terminal to v0.2024.02.20.08.01.stable_02

--- pkgs/warp-terminal.nix
+++ pkgs/warp-terminal.nix
@@ -4,10 +4,10 @@
 
 appimageTools.wrapType2 rec {
   name = "warp-terminal";
-  version = "v0.2024.02.20.08.01.stable_01";
+  version = "v0.2024.02.20.08.01.stable_02";
   src = fetchurl {
     url = "https://releases.warp.dev/stable/${version}/Warp-x86_64.AppImage";
-    hash = "sha256-541IHjrtUGzZwQh5+q4M27/UF2ZTqhznPX6ieh2nqUQ=";
+    hash = "sha256-p9aLcV20BDJIk4phjXXerSD9b2AwKdbjWSQIs5m7Wsc=";
   };
   extraPkgs = pkgs: [];
 }
