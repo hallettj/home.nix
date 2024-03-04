99e739ebb54babd7017a0441b093b70be6fc2c8f
Jesse Hallett <jesse@sitr.us>
2024-03-04 10:13:36 -0800

    enable clamav

--- nixos/yu/configuration.nix
+++ nixos/yu/configuration.nix
@@ -23,6 +23,12 @@
 
   virtualisation.docker.enable = true;
 
+  services.clamav = {
+    daemon.enable = true;
+    updater.enable = true;
+    updater.interval = "daily";
+  };
+
   services.avahi = {
     enable = true;
     # domainName = "attlocal.net"; # the AT&T router feels a need to make my network non-standard
