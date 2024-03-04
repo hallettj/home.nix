d98f542d35268b144c556e8733277dbbad7fb2e8
Jesse Hallett <jesse@sitr.us>
2024-03-04 10:14:41 -0800

    remove samba configuration

--- nixos/yu/configuration.nix
+++ nixos/yu/configuration.nix
@@ -29,50 +29,5 @@
     updater.interval = "daily";
   };
 
-  services.avahi = {
-    enable = true;
-    # domainName = "attlocal.net"; # the AT&T router feels a need to make my network non-standard
-    publish = {
-      enable = true;
-      addresses = true;
-      domain = true;
-      hinfo = true;
-      userServices = true;
-      workstation = true;
-    };
-    extraServiceFiles.smb = ''
-      <?xml version="1.0" standalone='no'?><!--*-nxml-*-->
-      <!DOCTYPE service-group SYSTEM "avahi-service.dtd">
-      <service-group>
-        <name replace-wildcards="yes">%h</name>
-        <service>
-          <type>_smb._tcp</type>
-          <port>445</port>
-        </service>
-      </service-group>
-    '';
-  };
-
-  # Samba server to share files over local network
-  services.samba = {
-    enable = true;
-    securityType = "user";
-    extraConfig = ''
-      guest account = nobody
-      map to guest = bad user
-      bind interfaces only = yes
-      interfaces = lo enp5s0 wlp4s0
-    '';
-    shares = {
-      public = {
-        path = "/srv/public";
-        browseable = "yes";
-        "read only" = true;
-        "guest ok" = "yes";
-      };
-    };
-  };
-
-  # Allows browsing Samba shares in Gnome
-  services.gvfs.enable = true;
+  services.avahi.enable = true;
 }
