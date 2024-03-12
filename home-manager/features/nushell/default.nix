06d7c845b0fd4cff6e6da92cce7390e97e755f76
Jesse Hallett <jesse@sitr.us>
2024-03-12 11:27:22 -0700

    update zoxide configuration for latest nushell version

--- home-manager/features/nushell/default.nix
+++ home-manager/features/nushell/default.nix
@@ -40,6 +40,27 @@ in
   # Change directories with fuzzy search
   programs.zoxide.enable = true;
 
+  # Temporarily set up zoxide nushell integration manually because the Home
+  # Manager 23.11 zoxide module is not updated for the latest nushell version.
+  # This is copied from 
+  # https://github.com/nix-community/home-manager/blob/3df2a80f3f85f91ea06e5e91071fa74ba92e5084/modules/programs/zoxide.nix
+  programs.zoxide.enableNushellIntegration = false;
+  programs.nushell = {
+    extraEnv = ''
+      let zoxide_cache = "${config.xdg.cacheHome}/zoxide"
+      if not ($zoxide_cache | path exists) {
+        mkdir $zoxide_cache
+      }
+      ${config.programs.zoxide.package}/bin/zoxide init nushell ${builtins.concatStringsSep " " config.programs.zoxide.options} |
+        str replace "def-env" "def --env" --all |  # https://github.com/ajeetdsouza/zoxide/pull/632
+        str replace --all "-- $rest" "-- ...$rest" |
+        save --force ${config.xdg.cacheHome}/zoxide/init.nu
+    '';
+    extraConfig = ''
+      source ${config.xdg.cacheHome}/zoxide/init.nu
+    '';
+  };
+
   # I'm not setting nushell as my login shell because it is not Posix, and it is
   # not super stable. Instead I'm configuring kitty to run nushell instead of my
   # login shell.
