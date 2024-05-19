295abf65e8a62870750d9ede0023561254969833
Jesse Hallett <jesse@sitr.us>
2024-05-19 15:35:21 -0700

    move waybar config into its own file

--- home-manager/features/niri/default.nix
+++ home-manager/features/niri/default.nix
@@ -5,39 +5,14 @@ let
   # is because relative paths are expanded after the flake source is copied to
   # a store path which would get us read-only store paths.
   dir = "${flakePath config}/home-manager/features/niri";
-
   niri-bin = "${pkgs.niri-stable}/bin/niri";
-
-  catppuccin-macchiato = {
-    rosewater = "f4dbd6";
-    flamingo = "f0c6c6";
-    pink = "f5bde6";
-    mauve = "c6a0f6";
-    red = "ed8796";
-    maroon = "ee99a0";
-    peach = "f5a97f";
-    yellow = "eed49f";
-    green = "a6da95";
-    teal = "8bd5ca";
-    sky = "91d7e3";
-    sapphire = "7dc4e4";
-    blue = "8aadf4";
-    lavender = "b7bdf8";
-    text = "cad3f5";
-    subtext1 = "b8c0e0";
-    subtext0 = "a5adcb";
-    overlay2 = "939ab7";
-    overlay1 = "8087a2";
-    overlay0 = "6e738d";
-    surface2 = "5b6078";
-    surface1 = "494d64";
-    surface0 = "363a4f";
-    base = "24273a";
-    mantle = "1e2030";
-    crust = "181926";
-  };
+  colors = (import ./colors.nix).catppuccin-macchiato;
 in
 {
+  imports = [
+    ./waybar.nix
+  ];
+
   # Nix packages configure Chrome and Electron apps to run in native Wayland
   # mode if this environment variable is set.
   home.sessionVariables.NIXOS_OZONE_WL = "1";
@@ -67,7 +42,7 @@ in
 
   programs.swaylock = {
     enable = true;
-    settings = with catppuccin-macchiato; {
+    settings = with colors; {
       color = mantle;
       font-size = 48;
       font = "Cantarell";
@@ -103,103 +78,6 @@ in
       show-failed-attempts = true;
     };
   };
-
-  programs.waybar =
-    let
-      notification-click-actions = {
-        on-click = "swaync-client -t -sw";
-        on-click-right = "swaync-client -d -sw";
-      };
-    in
-    {
-      enable = true;
-      settings = {
-        mainBar = {
-          layer = "top";
-          position = "top";
-          height = 30;
-
-          modules-left = [ "custom/niri-focused-window" ];
-          modules-center = [ "clock" "custom/notification" ];
-          modules-right = [ "tray" "network" ];
-
-          clock = {
-            format = "{:%a, %b %d  %H:%M}";
-          } // notification-click-actions;
-
-          "custom/niri-focused-window" =
-            let
-              jq-filter = ''
-                if . then
-                  { text: "\(.app_id) — \(.title)", alt: .app_id, class: ["focused-window"] }
-                else
-                  { text: "", alt: "", class: ["focused-window"] }
-                end
-              '';
-              jq = "${pkgs.jq}/bin/jq";
-              script = pkgs.writeShellScript "niri-focused-window" ''
-                ${niri-bin} msg --json focused-window | ${jq} --unbuffered --compact-output '${jq-filter}'
-              '';
-            in
-            {
-              tooltip = false;
-              return-type = "json";
-              exec = script.outPath;
-              interval = 1;
-            };
-
-          "custom/notification" = {
-            tooltip = false;
-            format = "{icon}";
-            format-icons = with catppuccin-macchiato; {
-              notification = "<span foreground='#${maroon}'></span>";
-              none = "";
-              dnd-notification = "<span foreground='#${maroon}'></span>";
-              dnd-none = "";
-              inhibited-notification = "<span foreground='#${maroon}'></span>";
-              inhibited-none = "";
-              dnd-inhibited-notification = "<span foreground='#${maroon}'></span>";
-              dnd-inhibited-none = "";
-            };
-            return-type = "json";
-            exec-if = "which swaync-client";
-            exec = "swaync-client -swb";
-            escape = true;
-          } // notification-click-actions;
-
-          tray.icon-size = 20;
-
-          network = {
-            format-wifi = "{icon}";
-            format-ethernet = "󰈀";
-            format-disconnected = "󰤮"; # An empty format will hide the module.
-            tooltip-format-wifi = "{ifname} [{essid}] {ipaddr}/{cidr} ({signalStrength}%)";
-            tooltip-format-ethernet = "{ifname} {ipaddr}/{cidr}";
-            tooltip-format-disconnected = "{ifname} disconnected";
-            format-icons = [ "󰤯" "󰤟" "󰤢" "󰤥" "󰤨" ];
-            max-length = 50;
-          } // (
-            let
-              rofi-wifi-menu = pkgs.fetchFromGitHub {
-                owner = "ericmurphyxyz";
-                repo = "rofi-wifi-menu";
-                rev = "d6debde6e302f68d8235ced690d12719124ff18e";
-                hash = "sha256-H+vBRdGcSDMKGLHhPB7imV148O8GRTMj1tZ+PLQUVG4=";
-              };
-            in
-            {
-              on-click = "${pkgs.bash}/bin/bash ${rofi-wifi-menu}/rofi-wifi-menu.sh";
-              on-click-right = "${pkgs.networkmanagerapplet}/bin/nm-connection-editor";
-            }
-          );
-        };
-      };
-      style = ./waybar.css;
-      systemd = {
-        enable = true;
-        target = "niri.service";
-      };
-    };
 
   services.blueman-applet.enable = true;
 
@@ -269,7 +147,7 @@ in
     Service = {
       Type = "simple";
       Restart = "always";
-      ExecStart = "${pkgs.swaybg}/bin/swaybg --color #363a4f"; # "Surface0" from Catppuccin Macchiato theme
+      ExecStart = "${pkgs.swaybg}/bin/swaybg --color #${colors.surface0}";
     };
     Install.WantedBy = [ "niri.service" ];
   };
---
+++ home-manager/features/niri/waybar.nix
@@ -0,0 +1,104 @@
+{ pkgs, ... }:
+
+let
+  niri-bin = "${pkgs.niri-stable}/bin/niri";
+  colors = (import ./colors.nix).catppuccin-macchiato;
+in
+{
+  programs.waybar =
+    let
+      notification-click-actions = {
+        on-click = "swaync-client -t -sw";
+        on-click-right = "swaync-client -d -sw";
+      };
+    in
+    {
+      enable = true;
+      settings = {
+        mainBar = {
+          layer = "top";
+          position = "top";
+          height = 30;
+
+          modules-left = [ "custom/niri-focused-window" ];
+          modules-center = [ "clock" "custom/notification" ];
+          modules-right = [ "tray" "network" ];
+
+          clock = {
+            format = "{:%a, %b %d  %H:%M}";
+          } // notification-click-actions;
+
+          "custom/niri-focused-window" =
+            let
+              jq-filter = ''
+                if . then
+                  { text: "\(.app_id) — \(.title)", alt: .app_id, class: ["focused-window"] }
+                else
+                  { text: "", alt: "", class: ["focused-window"] }
+                end
+              '';
+              jq = "${pkgs.jq}/bin/jq";
+              script = pkgs.writeShellScript "niri-focused-window" ''
+                ${niri-bin} msg --json focused-window | ${jq} --unbuffered --compact-output '${jq-filter}'
+              '';
+            in
+            {
+              tooltip = false;
+              return-type = "json";
+              exec = script.outPath;
+              interval = 1;
+            };
+
+          "custom/notification" = {
+            tooltip = false;
+            format = "{icon}";
+            format-icons = with colors; {
+              notification = "<span foreground='#${maroon}'></span>";
+              none = "";
+              dnd-notification = "<span foreground='#${maroon}'></span>";
+              dnd-none = "";
+              inhibited-notification = "<span foreground='#${maroon}'></span>";
+              inhibited-none = "";
+              dnd-inhibited-notification = "<span foreground='#${maroon}'></span>";
+              dnd-inhibited-none = "";
+            };
+            return-type = "json";
+            exec-if = "which swaync-client";
+            exec = "swaync-client -swb";
+            escape = true;
+          } // notification-click-actions;
+
+          tray.icon-size = 20;
+
+          network = {
+            format-wifi = "{icon}";
+            format-ethernet = "󰈀";
+            format-disconnected = "󰤮"; # An empty format will hide the module.
+            tooltip-format-wifi = "{ifname} [{essid}] {ipaddr}/{cidr} ({signalStrength}%)";
+            tooltip-format-ethernet = "{ifname} {ipaddr}/{cidr}";
+            tooltip-format-disconnected = "{ifname} disconnected";
+            format-icons = [ "󰤯" "󰤟" "󰤢" "󰤥" "󰤨" ];
+            max-length = 50;
+          } // (
+            let
+              rofi-wifi-menu = pkgs.fetchFromGitHub {
+                owner = "ericmurphyxyz";
+                repo = "rofi-wifi-menu";
+                rev = "d6debde6e302f68d8235ced690d12719124ff18e";
+                hash = "sha256-H+vBRdGcSDMKGLHhPB7imV148O8GRTMj1tZ+PLQUVG4=";
+              };
+            in
+            {
+              on-click = "${pkgs.bash}/bin/bash ${rofi-wifi-menu}/rofi-wifi-menu.sh";
+              on-click-right = "${pkgs.networkmanagerapplet}/bin/nm-connection-editor";
+            }
+          );
+        };
+      };
+      style = ./waybar.css;
+      systemd = {
+        enable = true;
+        target = "niri.service";
+      };
+    };
+}
