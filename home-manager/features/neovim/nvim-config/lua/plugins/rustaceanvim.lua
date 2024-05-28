20c7c6ce7dfa6e703eb104b23ccdbc1761b51c84
Jesse Hallett <jesse@sitr.us>
2024-05-28 13:34:44 -0700

    move rustaceanvim key bindings into plugin spec

--- home-manager/features/neovim/nvim-config/lua/plugins/rustaceanvim.lua
+++ home-manager/features/neovim/nvim-config/lua/plugins/rustaceanvim.lua
@@ -1,5 +1,19 @@
--- See key bindings in after/ftplugin/rust.lua
 return {
   'mrcjkb/rustaceanvim',
   version = '^4',
+  lazy = false,
+  keys = {
+    { '<leader>cc', ft = 'rust', function() vim.cmd.RustLsp('codeAction') end,                  desc = 'code actions at cursor or selection', mode = { 'n', 'x' }, silent = true },
+
+    { '<leader>dd', ft = 'rust', function() vim.cmd.RustLsp('debug') end,                       desc = 'debug target at cursor' },
+    { '<leader>D',  ft = 'rust', function() vim.cmd.RustLsp { 'debuggables', bang = true } end, desc = 'debug last target again' },
+
+    { '<leader>ld', ft = 'rust', function() vim.cmd.RustLsp('debuggables') end,                 desc = 'list debuggables' },
+    { '<leader>lr', ft = 'rust', function() vim.cmd.RustLsp('runnables') end,                   desc = 'list runnables' },
+
+    { '<leader>rr', ft = 'rust', function() vim.cmd.RustLsp('run') end,                         desc = 'run target at cursor' },
+    { '<leader>R',  ft = 'rust', function() vim.cmd.RustLsp { 'run', bang = true } end,         desc = 'run last target again' },
+
+    { 'gC',         ft = 'rust', function() vim.cmd.RustLsp('openCargo') end,                   desc = 'open Cargo.toml' },
+  },
 }
