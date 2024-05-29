c166702d4f850c103fb7f6fe900e1ffc4e32b5d9
Jesse Hallett <jesse@sitr.us>
2024-05-28 23:24:34 -0700

    update niri to unstable 2024-05-28

--- nixos/features/niri.nix
+++ nixos/features/niri.nix
@@ -1,6 +1,7 @@
-{ inputs, ... }:
+{ inputs, pkgs, ... }:
 
 {
   imports = [ inputs.niri.nixosModules.niri ];
   programs.niri.enable = true;
+  programs.niri.package = pkgs.niri-unstable; # remove this line to switch to latest stable niri release
 }
