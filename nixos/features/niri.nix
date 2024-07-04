8211325acb3a2fb8c60a78eedb0fa4ad6d45eb61
Jesse Hallett <jesse@sitr.us>
2024-07-04 13:26:52 -0700

    switch to niri stable v0.1.7, upgrade flake inputs

--- nixos/features/niri.nix
+++ nixos/features/niri.nix
@@ -1,7 +1,7 @@
-{ inputs, pkgs, ... }:
+{ inputs, ... }:
 
 {
   imports = [ inputs.niri.nixosModules.niri ];
   programs.niri.enable = true;
-  programs.niri.package = pkgs.niri-unstable; # remove this line to switch to latest stable niri release
+  # programs.niri.package = pkgs.niri-unstable; # remove this line to switch to latest stable niri release
 }
