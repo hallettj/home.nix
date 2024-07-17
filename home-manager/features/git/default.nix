da0e3a10f3d4c3600a7d761e83e2e74118ef5ed6
Jesse Hallett <jesse@sitr.us>
2024-07-17 13:58:01 -0700

    use difftastic to when running `git diff`

--- home-manager/features/git/default.nix
+++ home-manager/features/git/default.nix
@@ -1,11 +1,20 @@
-{ ... }:
+{ pkgs, ... }:
 
+let
+  difftastic = pkgs.difftastic;
+  difft = "${difftastic}/bin/difft";
+in
 {
+  home.packages = [
+    difftastic
+  ];
+
   programs.git = {
     enable = true;
     userName = "Jesse Hallett";
     userEmail = "jesse@sitr.us";
     signing.key = "A5CC2BE3";
+
     aliases = {
       di = "diff --cached";
       lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
@@ -13,6 +22,9 @@
       it = "!git init && read -p 'Committer email address for this repo: ' email && git config user.email $email && git commit -m 'initial commit' --allow-empty";
       up = "git fetch origin main:main";
     };
+
+    ignores = [ "*.swo" "*.swp" ];
+
     extraConfig = {
       user.useConfigOnly = true;
       push.default = "simple";
@@ -22,6 +34,14 @@
         autocrlf = "input";
         editor = "nvim";
       };
+
+      # Settings given in difftastic manual https://difftastic.wilfred.me.uk/git.html
+      diff.external = "${difft} --display side-by-side";
+      diff.tool = "difftastic";
+      difftool.prompt = false;
+      "difftool \"difftastic\"".cmd = ''${difft} "$LOCAL" "$REMOTE"'';
+      pager.difftool = true;
+
       rebase.autosquash = true;
       rerere = {
         enabled = true;
@@ -31,6 +51,5 @@
         defaultBranch = "main";
       };
     };
-    ignores = [ "*.swo" "*.swp" ];
   };
 }
