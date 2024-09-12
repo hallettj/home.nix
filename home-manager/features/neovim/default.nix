f8f7a46256acda56fdaa7de2ffb386b5915b752c
Jesse Hallett <jesse@sitr.us>
2024-09-12 10:27:28 -0700

    skip building nickel for now

--- home-manager/features/neovim/default.nix
+++ home-manager/features/neovim/default.nix
@@ -50,8 +50,10 @@ in
     neovide
 
     # language servers
-    nickel-lang-cli # to pair with the LSP
-    nickel-lang-lsp # Nickel LSP from my overlay
+    # disable nickel for now because I'm not using it, and it takes a long time
+    # to build
+    # nickel-lang-cli # to pair with the LSP
+    # nickel-lang-lsp # Nickel LSP from my overlay
     vscode-langservers-extracted
   ];
 
