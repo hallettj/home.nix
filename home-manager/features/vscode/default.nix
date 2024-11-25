a0619f15d313240eeba1e737b75634c49685bf21
Jesse Hallett <jesse@sitr.us>
2024-11-25 09:32:27 -0800

    workaround to get vscode to use gnome-keyring

--- home-manager/features/vscode/default.nix
+++ home-manager/features/vscode/default.nix
@@ -4,4 +4,13 @@
   programs.vscode = {
     enable = true;
   };
+
+  home.file.vscode-argv = {
+    target = ".vscode/argv.json";
+    text = builtins.toJSON {
+      enable-crash-reporter = true;
+      crash-reporter-id = "c93a0a9e-2861-4c1a-bda5-7cff36314259";
+      password-store = "gnome-libsecret"; # vscode isn't detecting gnome-keyring automatically
+    };
+  };
 }
