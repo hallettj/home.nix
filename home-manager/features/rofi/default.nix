89b3e14db9dc6b9df4912d60a8b5e75be8c00e23
Jesse Hallett <jesse@sitr.us>
2025-02-23 17:53:46 -0800

    run rofi in combi mode using a single hotkey

--- home-manager/features/rofi/default.nix
+++ home-manager/features/rofi/default.nix
@@ -8,7 +8,15 @@
     font = "mono 18"; # columns don't align with proportional fonts
     terminal = "${config.programs.kitty.package}/bin/kitty";
     extraConfig = {
+      combi-display-format = "{text}";
       show-icons = true;
+
+      # Available placeholders are {w}, {t}, {n}, {c}, {r}
+      # For a fancier display that includes app names try,
+      #
+      #     "{t} <span weight='light' size='small'><i>{c}</i></span>"
+      #
+      window-format = "{t}";
     };
   };
 }
