948625d0131c414c955506a6521910a24b9833c3
Jesse Hallett <jesse@sitr.us>
2025-03-04 17:07:25 -0800

    enable mini-align

--- home-manager/features/neovim/nvim-config/lua/plugins/mini.lua
+++ home-manager/features/neovim/nvim-config/lua/plugins/mini.lua
@@ -12,8 +12,8 @@ return {
       custom_textobjects = {
         a = treesitter { a = '@parameter.outer', i = '@parameter.inner' },
         B = treesitter { a = '@block.outer', i = '@block.inner' },
-        c = treesitter { a = '@comment.outer', i = '@comment.inner' },      -- only 'ac' works in Rust
-        d = treesitter { a = '@class.outer', i = '@class.inner' }, -- 'd' for "declaration"
+        c = treesitter { a = '@comment.outer', i = '@comment.inner' }, -- only 'ac' works in Rust
+        d = treesitter { a = '@class.outer', i = '@class.inner' },     -- 'd' for "declaration"
         f = treesitter { a = '@call.outer', i = '@call.inner' },
         i = treesitter { a = '@import.outer', i = '@import.inner' },
         t = treesitter { a = '@custom_capture.outer', i = '@custom_capture.inner' }, -- 'd' for "declaration"
@@ -25,6 +25,15 @@ return {
         R = treesitter { a = '@return.outer', i = '@return.inner' },
         s = treesitter { a = '@statement.outer', i = '@statement.inner' },   -- only 'as' works in Rust
         v = treesitter { a = '@assignment.outer', i = '@assignment.inner' }, -- 'v' for "variable declaration"; inner gets lhs or rhs depending on cursor position
+      },
+    }
+
+    -- To align a markdown table select the table, then type gA|<cr>
+    require('mini.align').setup {
+      -- Module mappings. Use `''` (empty string) to disable one.
+      mappings = {
+        start = '', -- default mapping, ga, conflicts with character info binding
+        start_with_preview = 'gA',
       },
     }
 
