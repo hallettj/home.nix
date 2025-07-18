4ef9dbdb656332487841d1f07122f1dbef3c38ad
Jesse Hallett <jesse@sitr.us>
2025-07-17 20:44:39 -0700

    install mullvad

--- nixos/yu/configuration.nix
+++ nixos/yu/configuration.nix
@@ -12,13 +12,15 @@
       ../common.nix
       ../features/docker.nix
       ../features/gaming.nix
-      ../features/vpn
     ];
 
   networking.hostName = "yu"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   services.avahi.enable = true;
+
+  # Connect to VPN by running $ mullvad connect
+  services.mullvad-vpn.enable = true;
 
   virtualisation.virtualbox.host.enable = true;
   users.extraGroups.vboxusers.members = [ "jesse" ];
