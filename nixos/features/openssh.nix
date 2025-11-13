2da8defcb52e2492f7044459eb012d1a54a8f0ba
Jesse Hallett <jesse@sitr.us>
2025-11-13 13:39:06 -0800

    my known hosts setup only works if keys are in the same repo

--- nixos/features/openssh.nix
+++ nixos/features/openssh.nix
@@ -12,6 +12,11 @@ let
         "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCj7ndNxQowgcQnjshcLrqPEiiphnt+VTTvDP6mHBL9j1aNUkY4Ue1gvwnGLVlOhGeYrnZaMgRK6+PKCUXaDbC7qtbW8gIkhL7aGCsOr/C56SJMy/BCZfxd1nWzAOxSDPgVsmerOBYfNqltV9/hWCqBywINIR+5dIg6JTJ72pcEpEjcYgXkE2YEFXV1JHnsKgbLWNlhScqb2UmyRkQyytRLtL+38TGxkxCflmO+5Z8CSSNY7GidjMIZ7Q4zMjA2n1nGrlTDkzwDCsw+wqFPGQA179cnfGWOWRVruj16z6XyvxvjJwbz0wQZ75XK5tKSb7FNyeIEs4TT4jk+S4dhPeAUC5y+bDYirYgM4GC7uEnztnZyaVWQ7B381AK4Qdrwt51ZqExKbQpTUNn+EjqoTwvqNj4kqx5QUCI0ThS/YkOxJCXmPUWZbhjpCg56i+2aB6CmK2JGhn57K5mj0MNdBXA4/WnwH6XoPWJzK5Nyu2zB3nAZp+S5hpQs+p1vN1/wsjk="
       ];
     };
+    "sr.ht" = {
+      publicKeys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK2aDX5hsoJLPmz3Rs2Tx2ymYJhDjZjR2Be2DImhu6Kk"
+      ];
+    };
     "borgbase" = {
       hostNames = [ "efjn7220.repo.borgbase.com" "kd9sk70h.repo.borgbase.com" "t5gkkxea.repo.borgbase.com" ];
       publicKeys = [
@@ -24,6 +29,11 @@ let
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG02b6lYr1EX3td0DXztqcxTRZYnz49gP+eDehx64R85"
         "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBED8eF1G8begDAXNLfKAy+vG1oG0Gfdhy4YvtKXJbwdoxUV4NyhFZlRQA7Ql3l1QKjZqsGlzNc27v3YKTfgr6Nk="
         "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDjtHmy/GxQBvBc/O22iYtO5H2pMnuXgpPT4k8t4KpOo78e6hXMku4DER/XxlyP7NopYFSycb/v5DG7OGE1mZAcQiGF8Fp4C+K+ghSYt+crjETMsMuBLAfo5JOQfwpBx6PE7Fpv8gui0D6F8DozWSKguf2T0z9jCHH3Lk9OVkhsO6PYFdRKaSxOuVfTUTaHDhJWBohRhbPRaPrrrWPQ2COg3KJ0o6CjKne7GpWe39I0tQsFxVSFXoCWA4lOwR6ssPfK2MncCFcUGYkXjsf4aOpPWFOmrwnMo/FK5s09BL4i4ydbFZiWf2d4Vw6RrwaaSM7AfggHrmeLeIK9JPXkP5M50PM7Sma0F1//x7HxjZM6gjMX7Re5GLHCCTPgYYKND57W2c6/NuKjszSzGbhRvxHeApXRIQ5qtwU1JBpyriTkDDvt0u2WxOZ9oZir84EoZcpyKpe6260fdzvdVHnI4Nn9RIcv3CPKIDu6Acu2FX/2+qk2fveUlPwvX1sFU7LuEk0="
+      ];
+    };
+    "hypatia.local" = {
+      publicKeys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPRAFMQkdPnEaYhOPP1TDa1/yxbb8xfRFzCaRq7E+BMX root@hypatia"
       ];
     };
   };
@@ -53,7 +63,7 @@ in
         publicKeyFile = pubKey name;
         extraHostNames = [ (name + ".local") ] ++ lib.optional (name == hostName) "localhost";
       })
-      hosts) //
+      (lib.filterAttrs (name: _: builtins.pathExists (pubKey name)) hosts)) //
     # And also with the values from otherKnownHosts, converted to the format
     # expected by programs.ssh.knownHosts.*
     builtins.listToAttrs (
