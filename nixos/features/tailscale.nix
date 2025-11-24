4821b4b648f48cdebfdfd3232ac304b1919cea0d
Jesse Hallett <jesse@sitr.us>
2025-11-23 23:14:26 -0800

    update flake inputs; install tailscale

---
+++ nixos/features/tailscale.nix
@@ -0,0 +1,15 @@
+{ pkgs, ... }:
+
+let
+  # Use unstable version to get security fix in 1.90.8
+  tailscale = pkgs.unstable.tailscale;
+in
+{
+  environment.systemPackages = [ tailscale ];
+
+  services.tailscale = {
+    enable = true;
+    package = tailscale;
+  };
+}
+
