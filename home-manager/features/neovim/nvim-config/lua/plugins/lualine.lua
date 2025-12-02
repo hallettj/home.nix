e66d692885eb1cbad7fdc233cf55549592bd82e6
Jesse Hallett <jesse@sitr.us>
2025-12-02 13:48:13 -0800

    display formatoptions in status line
    inspired by https://blog.ezyang.com/2010/03/vim-textwidth/

--- home-manager/features/neovim/nvim-config/lua/plugins/lualine.lua
+++ home-manager/features/neovim/nvim-config/lua/plugins/lualine.lua
@@ -2,7 +2,6 @@ return {
   'nvim-lualine/lualine.nvim',
   dependencies = {
     'nvim-tree/nvim-web-devicons',
-    'AndreM222/copilot-lualine'
   },
   config = function()
     -- Mode is indicated in status line instead
@@ -12,6 +11,13 @@ return {
       typescript = 'ts',
       typescriptreact = 'tsx',
     }
+
+    -- Define a consistent order for displaying formatoptions based on order in :h fo-table
+    local fo_order = { 't', 'c', 'r', 'o', '/', 'q', 'w', 'a', 'n', '2', 'v', 'b', 'l', 'm', 'M', 'B', '1', ']', 'j', 'p' }
+    local fo_order_index = {}
+    for i, char in ipairs(fo_order) do
+      fo_order_index[char] = i
+    end
 
     local function truncate(max_len)
       return function(str)
@@ -27,12 +33,18 @@ return {
       return alias or ft
     end
 
-    local function lsp_status()
-      if vim.g.statusbar_show_lsp_status == 1 then
-        return vim.lsp.buf.server_ready() and '✔️' or ''
-      else
-        return ''
+    local function format_options()
+      local fo = vim.api.nvim_get_option_value('formatoptions', {})
+      local chars = {}
+      for char in fo:gmatch('.') do
+        table.insert(chars, char)
       end
+      table.sort(chars, function(a, b)
+        local pos_a = fo_order_index[a] or 999
+        local pos_b = fo_order_index[b] or 999
+        return pos_a < pos_b
+      end)
+      return table.concat(chars, '')
     end
 
     -- Termtoggle provides multiple toggleable terminals that are accessed by
@@ -47,19 +59,19 @@ return {
         lualine_a = { 'mode' },
         lualine_b = {
           { 'branch', fmt = truncate(12) },
-          'diff', 'diagnostics'
+          'diff',
+          'diagnostics',
         },
         lualine_c = {
           termtoggle_number,
           'filename',
         },
         lualine_x = {
-          'copilot',
-          lsp_status,
           { 'filetype', fmt = format_filetype },
+          format_options,
         },
         lualine_y = { 'progress' },
-        lualine_z = { 'location' }
+        lualine_z = { 'location' },
       },
       inactive_sections = {
         lualine_a = {},
