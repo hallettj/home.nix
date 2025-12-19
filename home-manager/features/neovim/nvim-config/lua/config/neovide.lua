1169d52147ca93b58fa54f796ee7ae61293afb74
Jesse Hallett <jesse@sitr.us>
2025-12-18 21:00:22 -0800

    niri handles hiding mouse cursor instead of neovide

--- home-manager/features/neovim/nvim-config/lua/config/neovide.lua
+++ home-manager/features/neovim/nvim-config/lua/config/neovide.lua
@@ -8,6 +8,5 @@ if vim.g.neovide then
   vim.g.neovide_refresh_rate = 72
   vim.g.neovide_remember_window_position = false
   vim.g.neovide_remember_window_size = false
-  vim.g.neovide_hide_mouse_when_typing = true
   vim.g.neovide_scroll_animation_length = 0.12
 end
