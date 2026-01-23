40a0fc1db963e6eb4e84ad840d46a6e365b6020b
Jesse Hallett <jesse@sitr.us>
2026-01-23 13:02:08 -0800

    open neotest panel in vertical split

--- home-manager/features/neovim/nvim-config/lua/plugins/neotest.lua
+++ home-manager/features/neovim/nvim-config/lua/plugins/neotest.lua
@@ -10,20 +10,23 @@ return {
     'mrcjkb/rustaceanvim',
   },
   keys = {
-    { '<leader>tt', mode = 'n', function() require('neotest').run.run() end,  desc = 'run the nearest test' },
-    { '<leader>t%', mode = 'n', function() require('neotest').run.run(vim.fn.expand('%')) end,  desc = 'run all tests in current file' },
-    { '<leader>tS', mode = 'n', function() require('neotest').run.stop() end, desc = 'stop the nearest test' },
-    { '<leader>ta', mode = 'n', function() require('neotest').run.stop() end, desc = 'attach to the nearest test' },
-    { '<leader>to', mode = 'n', function() require('neotest').output.open() end, desc = 'show test output' },
-    { '<leader>tp', mode = 'n', function() require('neotest').output_panel.toggle() end, desc = 'toggle test panel' },
-    { '<leader>ts', mode = 'n', function() require('neotest').summary.toggle() end, desc = 'toggle test summary sidebar' },
-    { '<leader>tw', mode = 'n', function() require('neotest').watch.toggle() end, desc = 'start/stop watching test' },
+    { '<leader>tt', mode = 'n', function() require('neotest').run.run({ strategy = "dap" }) end, desc = 'run the nearest test' },
+    { '<leader>t%', mode = 'n', function() require('neotest').run.run(vim.fn.expand('%')) end, desc = 'run all tests in current file' },
+    { '<leader>tS', mode = 'n', function() require('neotest').run.stop() end,                  desc = 'stop the nearest test' },
+    { '<leader>ta', mode = 'n', function() require('neotest').run.stop() end,                  desc = 'attach to the nearest test' },
+    { '<leader>to', mode = 'n', function() require('neotest').output.open() end,               desc = 'show test output' },
+    { '<leader>tp', mode = 'n', function() require('neotest').output_panel.toggle() end,       desc = 'toggle test panel' },
+    { '<leader>ts', mode = 'n', function() require('neotest').summary.toggle() end,            desc = 'toggle test summary sidebar' },
+    { '<leader>tw', mode = 'n', function() require('neotest').watch.toggle() end,              desc = 'start/stop watching test' },
   },
   config = function()
     require('neotest').setup {
       adapters = {
         require('rustaceanvim.neotest')
-      }
+      },
+      output_panel = {
+        open = 'botright vsplit | vertical resize 80' -- open panel in vertical split
+      },
     }
   end
 }
