55ef501a32e1e9e9501e9a588e727125384bb995
Jesse Hallett <jesse@sitr.us>
2026-04-09 17:36:58 -0700

    update nvim-surround for v4

--- home-manager/features/neovim/nvim-config/lua/plugins/surround.lua
+++ home-manager/features/neovim/nvim-config/lua/plugins/surround.lua
@@ -1,10 +1,5 @@
 local dict = require 'dict'
 
--- Customize keymaps to avoid conflicts with leap.
---
--- The general rule is that if the key ends in "_line", the delimiter pair is
--- added on new lines. If the key ends in "_cur", the surround is performed
--- around the current line.
 local keymaps = {
   normal          = 'gz',
   normal_cur      = 'gzz',
@@ -23,14 +18,27 @@ return {
     "nvim-treesitter/nvim-treesitter",
     'nvim-treesitter/nvim-treesitter-textobjects',
   },
-  keys = dict.values(keymaps),
+  -- Customize keymaps to avoid conflicts with leap.
+  keys = {
+    { '<C-g>s', mode = { 'i' }, '<Plug>(nvim-surround-insert)',          desc = 'add delimiters arround cursor' },
+    { '<C-g>S', mode = { 'i' }, '<Plug>(nvim-surround-insert-line)',     desc = 'add delimiters arround cursor, on new lines' },
+    { 'gz',     mode = { 'n' }, '<Plug>(nvim-surround-normal)',          desc = 'add delimiters around a motion' },
+    { 'gzz',    mode = { 'n' }, '<Plug>(nvim-surround-normal-cur)',      desc = 'add delimiters around current line' },
+    { 'gZ',     mode = { 'n' }, '<Plug>(nvim-surround-normal-line)',     desc = 'add delimiters around a motion, on new lines' },
+    { 'gZZ',    mode = { 'n' }, '<Plug>(nvim-surround-normal-cur-line)', desc = 'add delimiters around current line, on new lines' },
+    { 'gz',     mode = { 'x' }, '<Plug>(nvim-surround-visual)',          desc = 'add delimiters arround selection' },
+    { 'gZ',     mode = { 'x' }, '<Plug>(nvim-surround-visual-line)',     desc = 'add delimiters arround selection, on new lines' },
+    { 'dz',     mode = { 'n' }, '<Plug>(nvim-surround-delete)',          desc = 'delete specified delimiters' },
+    { 'cz',     mode = { 'n' }, '<Plug>(nvim-surround-change)',          desc = 'change specified delimeters' },
+    { 'cZ',     mode = { 'n' }, '<Plug>(nvim-surround-change-line)',     desc = 'change change specified delimiters, putting replacements on new lines' },
+  },
   opts = {
-    keymaps = keymaps,
     highlight = {
       duration = 150, -- ms
     },
   },
   init = function()
+    vim.g.nvim_surround_no_mappings = true -- disable default bindings
     vim.cmd.highlight({ 'link', 'NvimSurroundHighlight', 'IncSearch' })
   end,
 }
