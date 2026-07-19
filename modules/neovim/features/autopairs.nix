e04d9da7099cece9487c0d74a33797fb89f4a682
Jesse Hallett <jesse@sitr.us>
2026-07-19 11:54:11 -0700

    try disabling autopairs setting that prevents autopairing

--- modules/neovim/features/autopairs.nix
+++ modules/neovim/features/autopairs.nix
@@ -5,8 +5,9 @@
       specs.autopairs = {
         data = pkgs.vimPlugins.nvim-autopairs;
         config = /* lua */ ''
-          require('nvim-autopairs').setup {
-            check_ts = true;
+          require("nvim-autopairs").setup {
+            check_ts = true, -- Treesitter support
+            enable_check_bracket_line = false, -- disables this heuristic: if next character is a close pair and it doesn't have an open pair in same line, then it will not add a close pair
           }
         '';
       };
